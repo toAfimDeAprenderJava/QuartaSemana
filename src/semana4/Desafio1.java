7cf23ace0abaec821212f5d809d42ff62a4bc668
brvnv <bruno.rdx@metalfan.com>
2018-10-31 15:43:45 -0300

    Restaurada parte do antigo código onde existiam alguns produtos. Estrutura será reconstruída nos próximos commits

--- src/semana4/Desafio1.java
+++ src/semana4/Desafio1.java
@@ -20,6 +20,20 @@ package semana4;
  */
 public class Desafio1{
     public static void main(String[] args) {
+    	Produto prod1=new Produto();
+    	prod1.setId();
+    	prod1.setNome("Secador de Cabelo");
+    	prod1.setMarca("Taiff");
+    	prod1.setCor("Preto");
+    	prod1.setTamanho(25);
+    	
+    	Produto prod2=new Produto();
+    	prod2.setId();
+    	prod2.setNome("Balcão de Padaria");
+    	prod2.setMarca("Genérico");
+    	prod2.setCor("azul");
+    	prod2.setTamanho(75);
+    }
     
 
     }
