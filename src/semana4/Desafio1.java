10f7b8a13ae7b6f71353fa73a216e9e6d4a0b02e
Takakuwa <729534@cognizant.com>
2018-10-31 07:44:38 -0300

    inicial commit

---
+++ src/semana4/Desafio1.java
@@ -0,0 +1,27 @@
+package semana4;
+
+/**
+ * 	Uma classe “produtos” com atributos
+ * 		- nome do produto
+ * 		- tamanho do produto
+ * 		- cor
+ * 		- marca
+ * 	Uma classe ”estoque” que tem um objeto de produto
+ * 		- quantidade
+ * 		- id
+ * 		- produto
+ * 	 Uma classe "funcionario" que tem os métodos
+ * 		-Cadastrar
+ * 		-Editar
+ * 		-Excluir
+ * 		-Consultar
+ *
+ *  mostre os metodos em execução
+ */
+pubic class Desafio1{
+
+    public static void main(String[] args) {
+
+
+    }
+}
