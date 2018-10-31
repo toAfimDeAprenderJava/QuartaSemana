67d5a9ae4be67c19a85a656f9d7ce1f9e6eab2e2
brvnv <bruno.rdx@metalfan.com>
2018-10-31 15:21:55 -0300

    Esse código vai me dar um tempo melhor que eu imaginava. É o commit inicial e o que eu tenho é algo que depois de tantos avanços ainda é um escopo de esquema de gerência de estoque.

--- src/semana4/Desafio1.java
+++ src/semana4/Desafio1.java
@@ -1,27 +1,26 @@
 package semana4;
 
 /**
- * 	Uma classe “produtos” com atributos
+ * 	Uma classe â€œprodutosâ€� com atributos
  * 		- nome do produto
  * 		- tamanho do produto
  * 		- cor
  * 		- marca
- * 	Uma classe ”estoque” que tem um objeto de produto
+ * 	Uma classe â€�estoqueâ€� que tem um objeto de produto
  * 		- quantidade
  * 		- id
  * 		- produto
- * 	 Uma classe "funcionario" que tem os métodos
+ * 	 Uma classe "funcionario" que tem os mÃ©todos
  * 		-Cadastrar
  * 		-Editar
  * 		-Excluir
  * 		-Consultar
  *
- *  mostre os metodos em execução
+ *  mostre os metodos em execuÃ§Ã£o
  */
-pubic class Desafio1{
-
+public class Desafio1{
     public static void main(String[] args) {
-
+    
 
     }
 }
---
+++ src/semana4/Estoque.java
@@ -0,0 +1,12 @@
+package semana4;
+
+class Estoque {
+	protected void add(int id, String nome, String marca, String cor, int tamanho, int quantidade) {
+	Produto prod[(id-1)]=new Produto();
+	prod.setId(id);
+	prod.setNome(nome);
+	prod.setMarca(marca);
+	prod.setCor(cor);
+	prod.setTamanho(tamanho);
+	prod.setQuantidade(quantidade);
+}
