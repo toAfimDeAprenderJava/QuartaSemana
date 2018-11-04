e29e255854a4ccf633c1d42b0b4f078b4f1afb71
brvnv <bruno.rdx@metalfan.com>
2018-11-04 21:05:41 -0200

    Pensei que o erro era por causa do debug que tinha começado e esqueci de reverter, não é. Parece que vou ter que usar algo chamado list, nos próximos commits vemos isso

--- src/semana4/Estoque.java
+++ src/semana4/Estoque.java
@@ -2,18 +2,16 @@ package semana4;
 
 class Estoque {
 	int id=0;
-	Produto[] prod=new Produto[];
+	Produto[] prod=new Produto[]; 
 
 	protected void add(String nome, String marca, String cor, int tamanho, int quantidade) {
 		++id;
-		System.out.println("Produto:" + id);
-		System.out.println("Nome:" + nome);
-//		prod[(id-1)]=new Produto();
-//		prod[(id-1)].setId(id);
-//		prod[(id-1)].setNome(nome);
-//		prod[(id-1)].setMarca(marca);
-//		prod[(id-1)].setCor(cor);
-//		prod[(id-1)].setTamanho(tamanho);
-//		prod[(id-1)].setQuantidade(quantidade);
+		prod[(id-1)]=new Produto();
+		prod[(id-1)].setId(id);
+		prod[(id-1)].setNome(nome);
+		prod[(id-1)].setMarca(marca);
+		prod[(id-1)].setCor(cor);
+		prod[(id-1)].setTamanho(tamanho);
+		prod[(id-1)].setQuantidade(quantidade);
 	}
 }
