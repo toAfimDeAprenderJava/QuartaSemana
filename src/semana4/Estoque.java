eb51cc08dcff909a57a8fb82be928a6c6ec1e228
brvnv <bruno.rdx@metalfan.com>
2018-11-04 20:17:56 -0200

    Corrigido jeito estranho de declarar nova instância de objeto, removido mais uma criação de instância para cada elemento (não faz sentido porque prod é uma instância de Produto, e é uma array). Devo estar no caminho de fazer algo funcionar, mas o erro ainda é o mesmo

--- src/semana4/Estoque.java
+++ src/semana4/Estoque.java
@@ -2,11 +2,10 @@ package semana4;
 
 class Estoque {
 	static int id=0; //quando a vontade prevalece
-	static Produto prod[] = new Produto;
+	static Produto prod[] = new Produto();
 
 	protected static void add(String nome, String marca, String cor, int tamanho, int quantidade) {
 		++id;
-		prod[(id-1)]=new Produto();
 	prod[(id-1)].setId(id);
 	prod[(id-1)].setNome(nome);
 	prod[(id-1)].setMarca(marca);
