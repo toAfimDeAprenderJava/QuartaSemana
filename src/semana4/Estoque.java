0a2d851ee0bebc42b89d6d6d1d5cf0f708de9c69
brvnv <bruno.rdx@metalfan.com>
2018-11-05 10:59:02 -0200

    Testes adicionados, mais verbose, estoque avisando que produto é maior que sua capacidade e o adicionando mesmo assim corrigido (solução escrita de forma temporária, preciso de resultado)

--- src/semana4/Estoque.java
+++ src/semana4/Estoque.java
@@ -5,6 +5,10 @@ class Estoque {
 	Produto[] prod=new Produto[100]; 
 
 	protected void add(String nome, String marca, String cor, int tamanho, int quantidade) {
+		if(tamanho>60) {
+			System.out.println("Não há espaço para produtos deste tamanho no estoque.");
+			return;
+		}
 		++id;
 		prod[(id-1)]=new Produto();
 		prod[(id-1)].setId(id);
@@ -13,5 +17,6 @@ class Estoque {
 		prod[(id-1)].setCor(cor);
 		prod[(id-1)].setTamanho(tamanho);
 		prod[(id-1)].setQuantidade(quantidade);
+		System.out.println("Produto id " + id + " adicionado ao estoque.");
 	}
 }
